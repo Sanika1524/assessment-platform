aeff1ae373f7aa180efe51ea1fbfff4451453c70
Sanika Gaikwad <sanikagaikwad094@gmail.com>
2025-06-09 12:56:15 +0530

    Fix: Added base config for Vite and redirects for Netlify

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // 🔥 ADD THIS LINE
   plugins: [react()],
-   json: {
-    stringify: false, // optional
+  json: {
+    stringify: false,
     namedExports: true
   }
 })
